e836cbd10708ee128fe2528c857c070442aa8813
ilya smagin <ilya-z1999@ya.ru>
2021-04-08 20:38:51 +0300

    React routing basename prop

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
   return (
     <div className="App flex justify-around flex-wrap">
       <div className="watch-face w-52 h-64 my-12 text-center subpixel-antialiased shadow-2xl rounded-3xl overflow-hidden">
-        <BrowserRouter>
+        <BrowserRouter basename="/Watch-widget">
           <Switch>
             <Route path="/details">
               <Details />
